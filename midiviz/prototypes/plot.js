ff5db0c8a5729c7c9607a97da2f4c79af46ae27c
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-13 22:54:13 -0400

    Ran a quick lint

--- midiviz/prototypes/plot.js
+++ midiviz/prototypes/plot.js
@@ -12,50 +12,53 @@ d3.select("body").append("svg")
 var svg = d3.select("svg");
 
 function genreHistogram(track) {
-    var mapping = populateNoteFrequencyMap(track);
-    mapping.sort((a, b) => b.count - a.count);
-    console.log(mapping);
+  var mapping = populateNoteFrequencyMap(track);
+  mapping.sort((a, b) => b.count - a.count);
+  console.log(mapping);
 
-    var xScale = d3.scaleBand()
-        .domain(mapping.map(function(d) { return d.note; }))
-        .range([padding, width - padding * 2])
-        .padding(.1);
+  var xScale = d3.scaleBand()
+    .domain(mapping.map(function(d) {
+      return d.note;
+    }))
+    .range([padding, width - padding * 2])
+    .padding(.1);
 
-    var yScale = d3.scaleLinear()
-        .domain([0, d3.max(mapping, function (d) { return d.count; })])
-        .range([height - padding, padding]);
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(mapping, function(d) {
+      return d.count;
+    })])
+    .range([height - padding, padding]);
 
-    svg.append("g")
-	    .attr("transform", "translate(0," + (height - padding) + ")")
-	    .call(d3.axisBottom(xScale))
-	    .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-35)");
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-35)");
 
-	// Draw y-axis
-    svg.append("g")
-    	.attr("transform", "translate(" + padding + ", 0)")
-        .call(d3.axisLeft(yScale));
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
 
-    svg.selectAll(".bar")
-        .data(mapping)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("fill", "#dc3912")
-        .attr("x", function (d) {
-            return xScale(d.note);
-        })
-        .attr("y", function (d) {
-            return yScale(d.count);
-        })
-        .attr("width", xScale.bandwidth())
-        .attr("height", function (d) {
-            return height - yScale(d.count) - padding;
-        });
+  svg.selectAll(".bar")
+    .data(mapping)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("fill", "#dc3912")
+    .attr("x", function(d) {
+      return xScale(d.note);
+    })
+    .attr("y", function(d) {
+      return yScale(d.count);
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) {
+      return height - yScale(d.count) - padding;
+    });
 
-    drawTitle("Note Histogram");
+  drawTitle("Note Histogram");
 }
 
 /**
@@ -77,20 +80,23 @@ function midiLoadCallback(obj) {
 function populateNoteFrequencyMap(track) {
   var mapping = []
   track.forEach(function(midiEvent) {
-      midiEvent.event.forEach(function(d) {
-          if (d.type == 9) {
-            var found = false;
-            for (var i = 0; i < mapping.length && !found; i++) {
-                if (mapping[i].note == d.data[0]) {
-                    mapping[i].count += 1;
-                    found = true;
-                }
-            }
-            if (!found) {
-                mapping.push({"note": d.data[0], "count": 1})
-            }
+    midiEvent.event.forEach(function(d) {
+      if (d.type == 9) {
+        var found = false;
+        for (var i = 0; i < mapping.length && !found; i++) {
+          if (mapping[i].note == d.data[0]) {
+            mapping[i].count += 1;
+            found = true;
           }
-      });
+        }
+        if (!found) {
+          mapping.push({
+            "note": d.data[0],
+            "count": 1
+          })
+        }
+      }
+    });
   });
   return mapping;
 }
@@ -101,12 +107,12 @@ function populateNoteFrequencyMap(track) {
  * @param {string} title - the title to be drawn
  */
 function drawTitle(title) {
-    svg.append("text")
-	    .attr("class", "title")
-	    .attr("dy", padding / 2)
-	    .attr("dx", ((width/2) - padding / 2))
-	    .style("text-anchor", "middle")
-	    .style("font-size", "20px")
-        .style("text-decoration", "underline")
-	    .text(title)
+  svg.append("text")
+    .attr("class", "title")
+    .attr("dy", padding / 2)
+    .attr("dx", ((width / 2) - padding / 2))
+    .style("text-anchor", "middle")
+    .style("font-size", "20px")
+    .style("text-decoration", "underline")
+    .text(title)
 }
