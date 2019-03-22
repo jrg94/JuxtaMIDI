35a0ee675c08aa233934a08083c190a170f78e5b
Stephen Wu <wujstephen@gmail.com>
2019-03-22 00:03:28 -0400

    Improved velocity over time graph to draw area between 2 lines of MIN/MAX velocity

--- midiviz/prototype/dashboard.js
+++ midiviz/prototype/dashboard.js
@@ -191,7 +191,7 @@ function graphVelocity() {
     .range([padding, width - padding * 2]);
 
   var yVelocityScale = d3.scaleLinear()
-    .domain([d3.min(timestamps, d => d.velocity), d3.max(timestamps, d => d.velocity)])
+    .domain([d3.min(timestamps, d => d.loVelocity - 15), d3.max(timestamps, d => d.hiVelocity + 15)])
     .range([height - padding, padding]);
 
   svg.append("g")
@@ -207,21 +207,36 @@ function graphVelocity() {
     .attr("transform", "translate(" + padding + ", 0)")
     .call(d3.axisLeft(yVelocityScale));
 
-  var line = d3.line()
-    .x(d => xTimeScale(d.time))
-    .y(d => yVelocityScale(d.velocity))
-    .curve(d3.curveMonotoneX)
-
   var subsets = keys.map(key => timestamps.filter(d => d.name == key));
+
+  var area = d3.area()
+    .x(d => xTimeScale(d.time))
+    .y0(d => yVelocityScale(d.loVelocity))
+    .y1(d => yVelocityScale(d.hiVelocity))
 
   svg.append("g")
     .selectAll("path")
     .data(subsets)
     .join("path")
-    .attr("fill", d => (d.length > 0 && "color" in d[0]) ? d[0].color : "black" )
-    // "black" graph should never appear, in theory, since it implies no notes in file
-    .style("mix-blend-mode", "multiply")
-    .attr("d", line);
+    .attr("stroke", d => (d.length > 0 && "color" in d[0]) ? d[0].color : "black" )
+    .attr("fill", d => d[0].color + "33")
+    .attr("d", area);
+
+  for (velocity of ["loVelocity", "hiVelocity"]) {
+    // These dots are invisible but are used for tooltips
+    svg.append("g")
+      .selectAll(".dot")
+      .data(timestamps)
+      .enter()
+      .append("circle")
+      .attr("class", "tipped")
+      .attr("fill", "transparent")
+      .attr("stroke", "transparent")
+      .attr("cx", d => xTimeScale(d.time))
+      .attr("cy", d => yVelocityScale(d[velocity]))
+      .attr("r", 3)
+      .attr("data-tippy-content", d => `${d.name}<br>time: ${d.time}<br>loVelocity: ${d.loVelocity}<br>hiVelocity: ${d.hiVelocity}`);
+  }
 
   drawTitle(svg, width / 2, height, padding, "Note Velocity");
 }
@@ -248,13 +263,14 @@ function getVelocityMapping() {
             }
           }
           if (existingTimestamp) {
-            existingTimestamp.velocity += d.data[1];
+            existingTimestamp.loVelocity = Math.min(d.data[1], existingTimestamp.loVelocity);
+            existingTimestamp.hiVelocity = Math.max(d.data[1], existingTimestamp.hiVelocity);
           } else {
             mapping.push({
               name: name,
               time: runningTime,
-              velocity: d.data[1],
-              note: noteLUT[d.data[0]],
+              loVelocity: d.data[1],
+              hiVelocity: d.data[1],
               color: midiFile.color
             });
           }
@@ -534,7 +550,8 @@ function applyTooltips() {
     arrow: true,
     animateFill: false,
     size: "small",
-    maxWidth: 200
+    maxWidth: 200,
+    interactiveBorder: 8
   })
 }
 
